69d739e3d27c63fa0ae435a5b6b670dd73d4021f
mbabic <mbabic@ualberta.ca>
2014-06-26 14:49:05 -0600

    HCDbHelper refactored to use HCDbTable to do table creation.

--- src/ca/mbabic/headphonecontroller/db/HCDbHelper.java
+++ src/ca/mbabic/headphonecontroller/db/HCDbHelper.java
@@ -16,62 +16,6 @@ public class HCDbHelper extends SQLiteOpenHelper {
 	 */
 	private static final int DB_VERSION = 1;
 	
-	/**
-	 * COMMAND table execution statement.
-	 */
-	private static final String createCommandTableStmt = 
-			"CREATE TABLE COMMAND(" 			+
-				"id int PRIMARY KEY NOT NULL, " 	+
-				"key text NOT NULL, "			+
-				"name text NOT NULL"			+
-			");";
-	
-	/**
-	 * CALLSTATE table creation statement.
-	 */
-	private static final String createCallStateTableStmt =
-			"CREATE TABLE CALLSTATE(" 			+
-				"id int PRIMARY KEY NOT NULL, " +
-				"name text NOT NULL" 			+
-			");";
-	
-	/**
-	 * COMMAND_CALLSTATES table -- the table encoding the call states
-	 * in which a command can be executed -- creation statement.
-	 */
-	private static final String createCommandCallStatesTableStmt =
-			"CREATE TABLE COMMAND_CALLSTATES("  					+
-				"command_id int, " 									+
-				"callstate_id int, " 								+
-				"FOREIGN KEY (command_id) REFERENCES COMMAND(id), "	+
-				"FOREIGN KEY (callstate_id) REFERENCE CALLSTATE(id)"+
-			");";
-	
-	/**
-	 * INPUTSEQUENCE table creation statement.
-	 */
-	private static final String createInputSequenceTableStmt = 
-			"CREATE TABLE INPUTSEQUENCE(" 		+
-				"id int PRIMARY KEY NOT NULL, " 	+
-				"key text NOT NULL, " 			+
-				"name text NOT NULL" 			+
-			");";
-	
-	/**
-	 * INPUTSEQUENCE_COMMANDS table -- the table encoding the commands
-	 * to be executed for the given input sequence in a particular call
-	 * state -- creation statement.
-	 */
-	private static final String createISCommandsTableStmt = 
-			"CREATE TABLE INPUTSEQUENCE_COMMANDS(" 	+
-				"inputsequence_id int, " 			+
-				"command_id int, " 					+
-				"callstate_id int, " 				+
-				"FOREIGN KEY (inputsequence_id) REFERENCES INPUTSEQUENCE(id), " +
-				"FOREIGN KEY (command_id) REFERENCES COMMAND(id), " +
-				"FOREIGN KEY (callstate_id) REFERENCES CALLSTATE(id)" +
-			");";
-	
 	public HCDbHelper(Context context, String name) {
 		super(context, name, null, DB_VERSION);
 	}
@@ -92,12 +36,33 @@ public class HCDbHelper extends SQLiteOpenHelper {
 	 * 		The database instance on which to execute the commands.
 	 */
 	private void executeCreationStatements(SQLiteDatabase db) {
+
+		HCDbTable table;
 		
-		db.execSQL(createCommandTableStmt);
-		db.execSQL(createCallStateTableStmt);
-		db.execSQL(createCommandCallStatesTableStmt);
-		db.execSQL(createInputSequenceTableStmt);
-		db.execSQL(createISCommandsTableStmt);
+		// Create CallState table.
+		table = new HCCallStateTable();
+		table.onCreate(db);
+		
+		// Create Command table.
+		table = new HCCommandTable();
+		table.onCreate(db);
+		
+		// Create InputSequence table.
+		table = new HCInputSequenceTable();
+		table.onCreate(db);
+		
+		// Create table encoding which commands can be called from which
+		// call states.
+		table = new HCCommandCallStateTable();
+		table.onCreate(db);
+		
+		// Create table encoding which commands are set for each input sequence
+		// given a call state.
+		table = new HCInputSequenceCommandsTable();
+		table.onCreate(db);
+		
+		// TODO: insert default values into the tables.
+
 	}
 
 }
