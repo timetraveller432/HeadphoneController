b4cf2c18f41ef9ab31f834608d0de29bae6b9ce2
mbabic <mbabic@ualberta.ca>
2014-06-27 10:06:07 -0600

    HCCommandTable.getDefaultValues() implemented.

--- src/ca/mbabic/headphonecontroller/db/HCCommandTable.java
+++ src/ca/mbabic/headphonecontroller/db/HCCommandTable.java
@@ -2,10 +2,17 @@ package ca.mbabic.headphonecontroller.db;
 
 import java.util.ArrayList;
 
+import static ca.mbabic.headphonecontroller.configuration.HCConfigConstants.*;
+
 import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
 
 public class HCCommandTable extends HCDbTable {		
+	
+	/**
+	 * Key for display name stored in db.
+	 */
+	public static final String DISPLAY_NAME_KEY = "display_name";
 	
 	public HCCommandTable() {
 		
@@ -13,8 +20,8 @@ public class HCCommandTable extends HCDbTable {
 		PRIMARY_KEY_NAME = "key";
 		CREATION_STMT =
 			"CREATE TABLE " + TABLE_NAME + "(" 	+
-				"key text PRIMARY KEY NOT NULL, " +
-				"name text NOT NULL"			+
+				PRIMARY_KEY_NAME + " text PRIMARY KEY NOT NULL, " +
+				DISPLAY_NAME_KEY + " text NOT NULL"			+
 			");";
 	}
 
@@ -24,10 +31,20 @@ public class HCCommandTable extends HCDbTable {
 		ArrayList<ContentValues> ret;
 		ContentValues cv;
 		
-		// 
-		cv = new ContentValues();
+		ret = new ArrayList<ContentValues>();
 		
-		return null;
+		for (String key : CMD_KEYS) {
+			
+			cv = new ContentValues();
+			
+			cv.put(PRIMARY_KEY_NAME, key);
+			cv.put(DISPLAY_NAME_KEY, CMD_DISPLAY_NAMES.get(key));
+			
+			ret.add(cv);
+			
+		}
+				
+		return ret;
 	}
 
 }
